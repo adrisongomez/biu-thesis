38f130b06607ad28abd720411f55d2a9ac6578ca
Adrison Gomez <agomez@resonance.nyc>
2025-09-14 16:41:32 -0400

    remove noe4j and make sure that prometheus and grafana are working

--- k8s/local/prometheus.tf
+++ k8s/local/prometheus.tf
@@ -1,5 +1,30 @@
 # prometheus.tf
 
+variable "grafana_admin_user" {
+  description = "The admin username for Grafana."
+  type        = string
+}
+
+variable "grafana_admin_password" {
+  description = "The admin password for Grafana."
+  type        = string
+  sensitive   = true # This prevents the value from being shown in logs
+}
+
+resource "kubernetes_secret" "grafana_credentials" {
+  metadata {
+    name      = "grafana-admin-credentials"
+    namespace = "monitoring"
+  }
+
+  # The 'data' field will be automatically base64 encoded by Terraform.
+  data = {
+    admin_user     = var.grafana_admin_user
+    admin_password = var.grafana_admin_password
+  }
+
+}
+
 resource "helm_release" "prometheus_stack" {
   name             = "prometheus-stack"
   repository       = "https://prometheus-community.github.io/helm-charts"
@@ -10,5 +35,9 @@ resource "helm_release" "prometheus_stack" {
 
   values = [
     file("${path.module}/values/prometheus-values.yaml")
+  ]
+  # Add this depends_on block
+  depends_on = [
+    kubernetes_secret.grafana_credentials
   ]
 }
