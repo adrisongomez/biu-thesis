8385bc0c6f46c6d615d1ed28b83c37cf0cf44a27
Adrison Gomez <agomez@resonance.nyc>
2025-09-20 22:29:11 -0400

    changes last

--- scripts/k6-simulate-traffic-custom.js
+++ scripts/k6-simulate-traffic-custom.js
@@ -3,11 +3,11 @@ import {sleep} from 'k6'
 
 export const options = {
     iterations: 10_000,   
-    vus: 50,
-    batch: 1_000,
+    vus: 10,
+    batch: 5_000,
 }
 
 export default function () {
     http.get("http://sample-service-custom.sample:5000/")
-    sleep(.01)
+    sleep(0.1)
 }
